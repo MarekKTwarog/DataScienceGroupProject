24454f501742c71acf5ea8bf00271f7cf93ae015
MarekKTwarog <135372084+MarekKTwarog@users.noreply.github.com>
2023-08-09 15:50:02 -0400

    Update DATASCIENCE PROJECT CODE.R
    Edited main branch to include both complete case and PSQI imputed dataframes

--- DATASCIENCE PROJECT CODE.R
+++ DATASCIENCE PROJECT CODE.R
@@ -51,14 +51,12 @@ library(corrplot)
 #' consider our missing PSQI data as MCAR or MAR. If the means are found to be
 #' significantly different from each other then this tells us that the missing NA
 #' values are not MCAR and thus a linear regression model cannot be used to
-#' impute themissing NA values for PSQ1.
+#' impute the missing NA values for PSQ1.
 
+# Create two data frames, one with NA values and one without NA values in Pittsburgh column
 na_rows <- is.na(liver_dfclean$Pittsburgh.Sleep.Quality.Index.Score)
 df_with_na <- liver_dfclean[na_rows, ]
 df_without_na <- liver_dfclean[!na_rows, ]
-# Create two data frames, one with NA values and one without NA values in Pittsburgh column
-df_with_na <- df[is.na(liver_dfclean$Pittsburgh.Sleep.Quality.Index.Score), ]
-df_without_na <- df[!is.na(liver_dfclean$Pittsburgh.Sleep.Quality.Index.Score), ]
 # Calculate means for variables in df_with_na
 mean_with_na <- colMeans(df_with_na[c("Athens.Insomnia.Scale", "Berlin.Sleepiness.Scale", "Epworth.Sleepiness.Scale")], na.rm = TRUE)
 mean_with_na
@@ -101,8 +99,16 @@ corrplot(correlation, type = "lower", diag = FALSE)
 lm_model <- lm(Pittsburgh.Sleep.Quality.Index.Score ~ Athens.Insomnia.Scale, data = liver_dfclean, na.action = na.exclude)
 liver_dfclean$PredictedPSQ1 <- predict(lm_model, newdata = liver_dfclean)
 
+#'Creating two datasets, one where only complete case values will be included and
+#'the other where missing values for PSQI will be imputed
+liver_dfcleanCC <- liver_dfclean
+liver_dfcleanIMP <- liver_dfclean
+
 # Replace NA values in 'PSQ1' with their respective 'y_predicted' values
-liver_dfclean$Pittsburgh.Sleep.Quality.Index.Score[is.na(liver_dfclean$Pittsburgh.Sleep.Quality.Index.Score)] <- liver_dfclean$PredictedPSQ1[is.na(liver_dfclean$Pittsburgh.Sleep.Quality.Index.Score)]
+liver_dfcleanIMP$Pittsburgh.Sleep.Quality.Index.Score[is.na(liver_dfcleanIMP$Pittsburgh.Sleep.Quality.Index.Score)] <- liver_dfcleanIMP$PredictedPSQ1[is.na(liver_dfcleanIMP$Pittsburgh.Sleep.Quality.Index.Score)]
+
+#Remove NA values from complete case dataframe
+liver_dfcleanCC <- na.omit(liver_dfcleanCC)
 
 #'Here we convert categorical variables into factors by first assigning all variables
 #'needing conversion to "categorical_var", then we implement a for loop which will
@@ -110,41 +116,65 @@ liver_dfclean$Pittsburgh.Sleep.Quality.Index.Score[is.na(liver_dfclean$Pittsburg
 #' then reassign the converted variable back to the liver_defclean dataset. The
 #' temporary 'col' variable aprt of the for loop is assigneed to each variable in
 #' the given list one by one until all variables have been converted to factors.
+#' This is done for both dataframes liver_dfcleanCC and liver_dfcleanIMP
 categorical_var <- c("Gender", "Liver.Diagnosis", "Recurrence.of.disease", "Rejection.graft.dysfunction",
                      "Any.fibrosis", "Renal.Failure", "Depression", "Corticoid", "Berlin.Sleepiness.Scale")
 for (col in categorical_var) {
-  liver_dfclean[[col]] <- factor(liver_dfclean[[col]])
+  liver_dfcleanIMP[[col]] <- factor(liver_dfcleanIMP[[col]])
+}
+
+for (col in categorical_var) {
+  liver_dfcleanCC[[col]] <- factor(liver_dfcleanCC[[col]])
 }
 
 #Checking to see if variables were correctly transformed
-glimpse(liver_dfclean)
+glimpse(liver_dfcleanIMP) #Looks good
+glimpse(liver_dfcleanCC) #Looks good
 
 #'Using the summary function to check for presence of any values that were not
 #'encoded as NA's but look to be out of place
-summary(liver_dfclean) #Looks good
+summary(liver_dfcleanIMP) #Looks good
+summary(liver_dfcleanCC) #Looks good
 
-#'Here , we convert sleep disturbance measures (PSQI, ESS, and AIS) to binary variables
-#' using the given clinically accepted threshold for each measure
-#' Berlin.Sleepiness.Scale (BSS) is already a binary variable, so will not convert it
-#' The new variables are added to the dataframe as new columns using the mutate() function from the tidyverse package
-liver_dfclean2 <- liver_dfclean %>%
-  mutate(Epworth_binary = ifelse(Epworth.Sleepiness.Scale > 10, 1, 0),                # for ESS, values above 10 indicate sleep disturbance and are changed to 1, else 0 (normal)
-         Pittsburgh_binary = ifelse(Pittsburgh.Sleep.Quality.Index.Score > 4, 1, 0),  # for PSQI, values above 4 indicate disturbed sleep and are changed to 1, else 0 (normal)
-         Athens_binary = ifelse(Athens.Insomnia.Scale > 5, 1, 0))                      # for AIS, values above 5 indicate sleep disturbance and are changed to 1, else 0 (normal)
+#'Here we convert PSQI, ESS and AIS, to binary variables like BSS based on the
+#'datasets description for values above a certain level. Epworth Sleepiness Scale
+#'total score greater than 10, indicating at least mild excessive daytime sleepiness
+#'(1 = yes, 0 = no), Pittsburgh Sleep Quality Index score greater than 4, indicating
+#'poor sleep quality (1 = yes, 0 = no),Athens Insomnia Scale total score greater than
+#'5, indicating the presence of insomnia, (1 = yes, 0 = no). The new variables are
+#'added to the dataframe as new columns using the mutate function from the tidyverse
+#'package.
+liver_dfcleanIMP <- liver_dfcleanIMP %>%
+  mutate(Epworth_binary = ifelse(Epworth.Sleepiness.Scale > 10, 1, 0),
+         Pittsburgh_binary = ifelse(Pittsburgh.Sleep.Quality.Index.Score > 4, 1, 0),
+         Athens_binary = ifelse(Athens.Insomnia.Scale > 5, 1, 0))
+
+liver_dfcleanCC <- liver_dfcleanCC %>%
+  mutate(Epworth_binary = ifelse(Epworth.Sleepiness.Scale > 10, 1, 0),
+         Pittsburgh_binary = ifelse(Pittsburgh.Sleep.Quality.Index.Score > 4, 1, 0),
+         Athens_binary = ifelse(Athens.Insomnia.Scale > 5, 1, 0))
 
 #Now we must convert the newly made binary variables to factors for further analysis
 categorical_var2 <- c("Epworth_binary", "Pittsburgh_binary", "Athens_binary")
+
 for (col in categorical_var2) {
-  liver_dfclean2[[col]] <- factor(liver_dfclean2[[col]])
+  liver_dfcleanIMP[[col]] <- factor(liver_dfcleanIMP[[col]])
+}
+
+for (col in categorical_var2) {
+  liver_dfcleanCC[[col]] <- factor(liver_dfcleanCC[[col]])
 }
 
 #checking to see if conversion to factors was successful
-glimpse(liver_dfclean2)#variables successfully converted to factors
+glimpse(liver_dfcleanIMP)#variables successfully converted to factors
+glimpse(liver_dfcleanCC)#variables successfully converted to factors
 
-#'The final set requires the removal of NA values from the dataset, this is done
+#'The final set requires the removal of NA values from the imputed dataset, this is done
 #'using the na.omit function
 
-liver_dfclean3 <- na.omit(liver_dfclean2)
+liver_dfcleanIMP <- na.omit(liver_dfcleanIMP)
+
+################################################################################
 
 #### Goal 2: Estimation of the prevalence of sleep disturbance
 
