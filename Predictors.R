2a30378ee1b34df5dd7eaf0548524289a7f43370
maaryahsalyani <135371572+maaryahsalyani@users.noreply.github.com>
2023-08-13 10:26:17 -0400

    Update Predictors.R

--- Predictors.R
+++ Predictors.R
@@ -61,63 +61,104 @@ summary(BSS.step.back.CC)
 
 
 
-#### Goal 3B: find the predictors associated with sleep disturbance for the liver_dfcleanIMPrmNA data frame
+#### Goal 3B: find the predictors associated with sleep disturbance for the liver_dfcleanPMM_IMPrmNA data frame
+
+# Fit the full logistic regression model with all predictors
+# logistic regression model was used instead of the linear model because we have binary variables.
+PSQI_full_IMPrmNA <- glm(Pittsburgh_binary~
+                          Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
+                          Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+                          Renal.Failure + Depression + Corticoid, data = na.omit(liver_dfcleanPMM_IMPrmNA), family = "binomial")
+
+# using the glimpse function to view a concise overview of the data frame
+glimpse(PSQI_full_IMPrmNA)
+# using the stepAIC function to perform the backwards stepwise selection process which eliminates predictors on the basis of AIC
+PSQI.step.back.IMPrmNA <- stepAIC(PSQI_full_IMPrmNA)
+summary(PSQI.step.back.IMPrmNA)
+
+# finding the predictors associated with sleep for the ESS measure
+ESS_full_IMPrmNA <- glm(Epworth_binary~
+                         Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
+                         Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+                         Renal.Failure + Depression + Corticoid, data = na.omit(liver_dfcleanPMM_IMPrmNA), family = "binomial")
+# using the glimpse function to view a concise overview of the data frame
+glimpse(ESS_full_IMPrmNA)
+# using the stepAIC function to perform the backwards stepwise selection process which eliminates predictors on the basis of AIC
+ESS.step.back.IMPrmNA <- stepAIC(ESS_full_IMPrmNA)
+
+summary(ESS.step.back.IMPrmNA)
+
+# finding the predictors associated with sleep for the AIS measure
+AIS_full_IMPrmNA <- glm(Athens_binary~
+                         Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
+                         Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+                         Renal.Failure + Depression + Corticoid, data = na.omit(liver_dfcleanPMM_IMPrmNA), family = "binomial")
+# using the glimpse function to view a concise overview of the data frame
+glimpse(AIS_full_IMPrmNAI)
+# using the stepAIC function to perform the backwards stepwise selection process which eliminates predictors on the basis of AIC
+AIS.step.back.IMPrmNA <- stepAIC(AIS_full_IMPrmNA)
+summary(AIS.step.back.IMPrmNA)
+
+
+# finding the predictors associated with sleep for the BSS measure
+BSS_full_IMPrmNA <- glm(Berlin.Sleepiness.Scale~
+                         Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
+                         Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+                         Renal.Failure + Depression + Corticoid, data = na.omit(liver_dfcleanPMM_IMPrmNA), family = "binomial")
+# using the glimpse function to view a concise overview of the data frame
+glimpse(BSS_full_IMPrmNA)
+# using the stepAIC function to perform the backwards stepwise selection process which eliminates predictors on the basis of AIC
+BSS.step.back.IMPrmNA <- stepAIC(BSS_full_IMPrmNA)
+summary(BSS.step.back.IMPrmNA)
+
+
+
+#### Goal 3C: find the predictors associated with sleep disturbance for the liver_noPSQI_rmNA data frame
 
 #Loading dataset
 liver_df <- read.csv("project_data.csv")
 
 #Loading packages for removal of weak predictors
 
-library(dplyr)
-install.packages("carData")
-library(carData)
-install.packages("MASS")
-library(MASS)
-
-
 # Fit the full logistic regression model with all predictors
 # logistic regression model was used instead of the linear model because we have binary variables.
-PSQI_full_rmNA <- glm(Pittsburgh_binary~
-                   Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
-                   Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
-                   Renal.Failure + Depression + Corticoid, data = na.omit(liver_dfcleanPW_IMPrmNA), family = "binomial")
-# using the glimpse function to view a concise overview of the data frame
-glimpse(liver_dfcleanPW_IMPrmNA)
-# using the stepAIC function to perform the backwards stepwise selection process which eliminates predictors on the basis of AIC
-PSQI.step.back.rmNA <- stepAIC(PSQI_full_rmNA)
-summary(PSQI.step.back.rmNA)
+PSQI_full_noPSQI <- glm(Pittsburgh_binary~
+                      Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
+                      Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+                      Renal.Failure + Depression + Corticoid, data = na.omit(liver_noPSQI_rmNA), family = "binomial")
+#ERROR: Pittsburgh_binary not found since PSQI was removed from this dataframe
 
 # finding the predictors associated with sleep for the ESS measure
-ESS_full_rmNA <- glm(Epworth_binary~
-                  Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
-                  Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
-                  Renal.Failure + Depression + Corticoid, data = na.omit(liver_dfcleanPW_IMPrmNA), family = "binomial")
+ESS_full_noPSQI <- glm(Epworth_binary~
+                     Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
+                     Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+                     Renal.Failure + Depression + Corticoid, data = na.omit(liver_noPSQI_rmNA), family = "binomial")
 # using the glimpse function to view a concise overview of the data frame
-glimpse(ESS_full_rmNA)
+glimpse(ESS_full_noPSQI)
 # using the stepAIC function to perform the backwards stepwise selection process which eliminates predictors on the basis of AIC
-ESS.step.back.rmNA <- stepAIC(ESS_full_rmNA)
+ESS.step.back.noPSQI <- stepAIC(ESS_full_noPSQI)
 
-summary(ESS.step.back.rmNA)
+summary(ESS.step.back.noPSQI)
 
 # finding the predictors associated with sleep for the AIS measure
-AIS_full_rmNA <- glm(Athens_binary~
-                  Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
-                  Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
-                  Renal.Failure + Depression + Corticoid, data = na.omit(liver_dfcleanPW_IMPrmNA), family = "binomial")
+AIS_full_noPSQI <- glm(Athens_binary~
+                     Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
+                     Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+                     Renal.Failure + Depression + Corticoid, data = na.omit(liver_noPSQI_rmNA), family = "binomial")
 # using the glimpse function to view a concise overview of the data frame
-glimpse(AIS_full_rmNA)
+glimpse(AIS_full_noPSQI)
 # using the stepAIC function to perform the backwards stepwise selection process which eliminates predictors on the basis of AIC
-AIS.step.back.rmNA <- stepAIC(AIS_full_rmNA)
-summary(AIS.step.back.rmNA)
+AIS.step.back.noPSQI <- stepAIC(AIS_full_noPSQI)
+summary(AIS.step.back.noPSQI)
 
 
 # finding the predictors associated with sleep for the BSS measure
-BSS_full_rmNA <- glm(Berlin.Sleepiness.Scale~
-                  Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
-                  Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
-                  Renal.Failure + Depression + Corticoid, data = na.omit(liver_dfcleanPW_IMPrmNA), family = "binomial")
+BSS_full_noPSQI <- glm(Berlin.Sleepiness.Scale~
+                     Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
+                     Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+                     Renal.Failure + Depression + Corticoid, data = na.omit(liver_noPSQI_rmNA), family = "binomial")
 # using the glimpse function to view a concise overview of the data frame
-glimpse(BSS_full_rmNA)
+glimpse(BSS_full_noPSQI)
 # using the stepAIC function to perform the backwards stepwise selection process which eliminates predictors on the basis of AIC
-BSS.step.back.rmNA <- stepAIC(BSS_full_rmNA)
-summary(BSS.step.back.rmNA)
+BSS.step.back.noPSQI <- stepAIC(BSS_full_noPSQI)
+summary(BSS.step.back.noPSQI)
