b460a1b60dd2f8c27e3930cce334adfa2ae187fc
maaryahsalyani <135371572+maaryahsalyani@users.noreply.github.com>
2023-08-15 12:43:05 -0400

    Update DATASCIENCE PROJECT CODE.R

--- DATASCIENCE PROJECT CODE.R
+++ DATASCIENCE PROJECT CODE.R
@@ -392,7 +392,6 @@ summary(BSS.step.back.CC)
 #### Goal 3B: find the predictors associated with sleep disturbance for the liver_dfcleanPMM_IMPrmNA data frame
 
 # Fit the full logistic regression model with all predictors
-# logistic regression model was used instead of the linear model because we have binary variables.
 PSQI_full_IMPrmNA <- glm(Pittsburgh_binary~
                           Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
                           Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
@@ -446,17 +445,8 @@ summary(BSS.step.back.IMPrmNA)
 #Loading dataset
 liver_df <- read.csv("project_data.csv")
 
-#Loading packages for removal of weak predictors
-
-# Fit the full logistic regression model with all predictors
-# logistic regression model was used instead of the linear model because we have binary variables.
-PSQI_full_noPSQI <- glm(Pittsburgh_binary~
-                      Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
-                      Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
-                      Renal.Failure + Depression + Corticoid, data = na.omit(liver_noPSQI_rmNA), family = "binomial")
-#ERROR: Pittsburgh_binary not found since PSQI was removed from this dataframe
-
 # finding the predictors associated with sleep for the ESS measure
+# Fit the full logistic regression model with all predictors
 ESS_full_noPSQI <- glm(Epworth_binary~
                      Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis +
                      Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
